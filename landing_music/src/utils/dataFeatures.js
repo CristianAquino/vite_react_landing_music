be1d8b956441e0770161deeb94d0a990b64a740f
cristianaquino <ycristian10@gmail.com>
2022-12-01 11:50:43 -0500

    utils

---
+++ landing_music/src/utils/dataFeatures.js
@@ -0,0 +1,16 @@
+const features = [
+  {
+    icon: "https://raw.githubusercontent.com/ZainRk/Youtube-Music-App/master/src/img/Group%202.png",
+    title: "For Live Music",
+  },
+  {
+    icon: "https://raw.githubusercontent.com/ZainRk/Youtube-Music-App/master/src/img/music%20icon.png",
+    title: "For Daily Music",
+  },
+  {
+    icon: "https://raw.githubusercontent.com/ZainRk/Youtube-Music-App/master/src/img/Group%204.png",
+    title: "For Artists",
+  },
+];
+
+export default features;
