903483eca9f23026df780046a87744839d30cc5f
cristianaquino <ycristian10@gmail.com>
2022-12-03 01:20:33 -0500

    utils

---
+++ landing_music/src/utils/testimonialsData.js
@@ -0,0 +1,26 @@
+export const testimonialsData = [
+  {
+    image:
+      "https://raw.githubusercontent.com/ZainRk/FitClub-Starter/master/src/assets/t-image1.png",
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: "MATHEW HENDRICKSON",
+    status: "ENTREPRENEUR",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/ZainRk/FitClub-Starter/master/src/assets/t-image2.jpg",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
+    name: "JOHN KEVIN",
+    status: "COACH",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/ZainRk/FitClub-Starter/master/src/assets/t-image3.jpg",
+    review:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem",
+    name: "FRANKLIN",
+    status: "CUSTOMER",
+  },
+];
