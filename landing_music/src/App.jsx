b140c53adf9c47f13fa270d36d0bcf2f545cade6
cristianaquino <ycristian10@gmail.com>
2022-12-06 16:58:23 -0500

    index

--- landing_music/src/App.jsx
+++ landing_music/src/App.jsx
@@ -5,6 +5,7 @@ import PageMusic from "./pages/PageMusic";
 import Layout from "./pages/Layout";
 import routes from "./utils/routesPage";
 import PageFitnes from "./pages/PageFitnes";
+import SocialMedia from "./pages/SocialMedia";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path={routes.home} element={<Layout />} />
       <Route path={routes.landingPageMusic} element={<PageMusic />} />
       <Route path={routes.landingPageFitnes} element={<PageFitnes />} />
+      <Route path={routes.socialMedia} element={<SocialMedia />} />
       <Route
         path={routes.error}
         element={<Navigate replace to={routes.home} />}
