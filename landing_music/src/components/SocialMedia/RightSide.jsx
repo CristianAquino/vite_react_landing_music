64a9325ddd389fe69c4eb53ea798106d37c7e99e
cristianaquino <ycristian10@gmail.com>
2022-12-06 19:02:57 -0500

    side

--- landing_music/src/components/SocialMedia/RightSide.jsx
+++ landing_music/src/components/SocialMedia/RightSide.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import ShareModal from "./ShareModal";
+import TrendCard from "./TrendCard";
 import {
   AiOutlineComment,
   AiFillHome,
   AiOutlineSetting,
   AiOutlineBell,
 } from "react-icons/ai";
-import TrendCard from "./TrendCard";
 
 const RightSide = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     // rightSide
     <div className="flex flex-col gap-[2rem]">
@@ -18,9 +21,13 @@ const RightSide = () => {
         <AiOutlineComment className="cursor-pointer" />
       </div>
       <TrendCard />
-      <button className="flex items-center justify-center text-white border-none rounded-[0.5rem] bg-buttonBg h-[2rem] px-[20px] hover:cursor-pointer ">
+      <button
+        className="flex items-center justify-center text-white border-none rounded-[0.5rem] bg-buttonBg h-[2rem] px-[20px] hover:cursor-pointer "
+        onClick={() => setModalOpen(true)}
+      >
         Share
       </button>
+      <ShareModal modalOpened={modalOpen} setModalOpened={setModalOpen} />
     </div>
   );
 };
